40bc183b4b9a1d6cb3ebef934a142c8ff75aff9c
dly <1083404373@qq.com>
2021-05-22 11:41:40 +0800

    Initial commit

---
+++ ch2/base_operator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/22 11:40
+# @Author  : dly
+# @File    : base_operator.py
+# @Desc    :
+
+import torch
+
+x = torch.empty(5, 3)
+print(x)
