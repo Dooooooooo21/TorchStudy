776d9655f10f70620423568d95af18e7b13fad26
dly <1083404373@qq.com>
2021-07-30 00:46:34 +0800

    网络搭建和参数

--- ch2/base_operator.py
+++ ch2/base_operator.py
@@ -6,20 +6,42 @@
 # @Desc    :
 
 import torch
+from torch import nn
 
-x = torch.empty(5, 3)
-print(x)
 
-print(x.size())
-print(x.shape)
+def tensor_exe():
+    x = torch.empty(5, 3)
+    print(x)
+
+    print(x.size())
+    print(x.shape)
+
 
 # 梯度
-y = torch.rand(2, 2, requires_grad=True)
-print(y)
-print(y.grad_fn)
+def grad_exe():
+    y = torch.rand(2, 2, requires_grad=True)
+    print(y)
+    print(y.grad_fn)
 
-z = y + 2
-print(z)
-print(z.grad_fn)
+    z = y + 2
+    print(z)
+    print(z.grad_fn)
 
-print(y.is_leaf, z.is_leaf)
+    print(y.is_leaf, z.is_leaf)
+
+
+# 网络
+def net_exe():
+    net = nn.Sequential()
+    net.add_module('linear', nn.Linear(6, 1))
+
+    print(net)
+    print('-' * 20)
+    print(net.parameters())
+
+    for para in net.parameters():
+        print(para)
+
+
+if __name__ == '__main__':
+    net_exe()
