83b276cfbb833d6f7bec9908e1e98072c7634b3d
dly <1083404373@qq.com>
2021-08-01 17:23:17 +0800

    卷积

---
+++ ch5/cnn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/1 16:37
+# @Author  : dly
+# @File    : cnn.py
+# @Desc    : 卷积
+
+import torch
+from torch import nn
+
+"""
+1. 卷积计算
+2. 特征图、感受野
+3. 填充、步幅
+4. 多输入通道, 1 * 1 卷积
+5. 池化
+6. LeNet, AlexNet, VGG
+"""
+
+
+# 定义一个函数来计算卷积层。它对输入和输出做相应的升维和降维
+def comp_conv2d(conv2d, X):
+    # (1, 1)代表批量大小和通道数（“多输入通道和多输出通道”一节将介绍）均为1
+    X = X.view((1, 1) + X.shape)
+    Y = conv2d(X)
+    return Y.view(Y.shape[2:])  # 排除不关心的前两维：批量和通道
+
+
+if __name__ == '__main__':
+    # 注意这里是两侧分别填充1行或列，所以在两侧一共填充2行或列
+    conv2d = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, padding=1)
+
+    X = torch.rand(8, 8)
+    print(comp_conv2d(conv2d, X).shape)
